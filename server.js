0920f80d677d884fefb01a8dbcead233891bb48a
kelvin9314 <kelvin9314@gmail.com>
2022-09-27 09:55:34 +0800

    build: set up TypeScript with Express server

--- server.js
+++ server.js
@@ -1,133 +1,60 @@
-const express = require('express')
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const jwt_decode = require('jwt-decode');
 const dotenv = require('dotenv');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const multer  = require('multer')
-const upload = multer()
-
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const upload = multer();
 dotenv.config();
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-
-const path = require('path').join(__dirname, './frontend/build')
-app.use(express.static(path))
-
-// app.use(function(req, res, next) {
-//   res.header('Content-Type', 'application/json;charset=UTF-8')
-//   res.header('Access-Control-Allow-Credentials', true)
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   )
-//   next()
-// })
-
+const path = require('path').join(__dirname, './frontend/build');
+app.use(express_1.default.static(path));
 app.get('/api', (req, res) => {
-  res.status(200).send('Hello World, this is API route')
-})
-
-app.post('/api/post', (req, res) =>{
-  console.log(req.body)
-  // res.sendStatus(200)
-  res.status(200).send({
-    message: 'OK',
-    data: req.body
-  });
-
-})
-
-// NOTE: multipart/form-data
-app.post('/f2e/Login',upload.array(),  (req, res) => {
-  let formData = req.body;
-  console.log('form data', formData);
-
-  if (!formData?.token){
-    res.redirect('/home')
-    return
-  }
-
-  try {
-    const decoded = jwt_decode(formData?.token)
-    console.log('decoded')
-    console.log(decoded)
-
-    const isTokenExpired = new Date(decoded.exp * 1000) < new Date()
-    console.log(isTokenExpired)
-    if (isTokenExpired || !decoded?.account || !decoded?.lang || !decoded?.uid) {
-      res.redirect('/home')
-    }
-    // const diffInMins = Math.floor(Math.abs(decoded.exp * 1000 - new Date().getTime()) / (1000 * 60))
-    // const diffInMins = 1
-    // res.append('Set-Cookie', `token=${formData.token}`,{ expires: diffInMins, maxAge: diffInMins })
-
-
-    // FIXME 先寫死 logt 這個cookie 有效時間為 1 分鐘
-     res.cookie('logt', formData?.token, { expires: new Date(Date.now() + 60 * 1000)});
-      //  res.cookie('logt', formData?.token, { expires: new Date(decoded.exp * 1000)});
-
-    res.redirect('/home')
-  } catch (err) {
-    console.log(err?.message)
-    res.redirect('/home')
-
-  }
-
-
-})
-
-// app.get('/f2e/get-cookies', (req, res, next) => {
-//   console.log(req)
-//   console.log(req.cookies);
-//   const token = req.cookies?.token
-//   if(!token) {
-//     res.clearCookie()
-//     res.end()
-//     return
-//   }
-
-//   try {
-//     const decoded = jwt_decode(token)
-//     console.log('decoded')
-//     console.log(decoded)
-
-//     const isTokenExpired = new Date(decoded.exp * 1000) < new Date()
-//     if (isTokenExpired || !decoded?.account || !decoded?.lang || !decoded?.uid) {
-//       // res.redirect('/home')
-//       res.clearCookie()
-//       res.end()
-//     }
-
-//   } catch (err) {
-//     console.log(err?.message)
-//   }
-
-//   res.json({token });
-// });
-
-// app.get('/f2e/clear-cookies', (req, res, next) => {
-//   console.log(req)
-
-//   res.clearCookie()
-
-//   res.json({
-//     message: "ok"
-//   })
-// });
-
-
-
-app.get('*', function (req,res) {
-  res.sendFile(path + "/index.html");
+    res.status(200).send('Hello World, this is API route');
 });
-
-
+app.post('/api/post', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({
+        message: 'OK',
+        data: req.body
+    });
+});
+app.post('/f2e/Login', upload.array(), (req, res) => {
+    let formData = req.body;
+    console.log('form data', formData);
+    if (!(formData === null || formData === void 0 ? void 0 : formData.token)) {
+        res.redirect('/home');
+        return;
+    }
+    try {
+        const decoded = jwt_decode(formData === null || formData === void 0 ? void 0 : formData.token);
+        console.log('decoded');
+        console.log(decoded);
+        const isTokenExpired = new Date(decoded.exp * 1000) < new Date();
+        console.log(isTokenExpired);
+        if (isTokenExpired || !(decoded === null || decoded === void 0 ? void 0 : decoded.account) || !(decoded === null || decoded === void 0 ? void 0 : decoded.lang) || !(decoded === null || decoded === void 0 ? void 0 : decoded.uid)) {
+            res.redirect('/home');
+        }
+        res.cookie('logt', formData === null || formData === void 0 ? void 0 : formData.token, { expires: new Date(Date.now() + 60 * 1000) });
+        res.redirect('/home');
+    }
+    catch (err) {
+        res.redirect('/home');
+    }
+});
+app.get('*', function (req, res) {
+    res.sendFile(path + "/index.html");
+});
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
